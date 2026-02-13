ba2eba24851e1a0ef85dd8cd42cd1b0e606c5b4b
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-02-13 07:12:20 +0600

    HEi

---
+++ Array/vec.rs
@@ -0,0 +1,15 @@
+// fn main() {
+//     let arr: [i32; 3] = [1, 2, 3];
+
+//     let mut vec: Vec<i32> = vec![1, 2, 3];
+//     vec.push(4);
+// }
+
+fn main() {
+    let arr: [i32; 3] = [1, 2, 3];
+
+    let mut vec: Vec<i32> = vec![1, 2, 3];
+    vec.push(5);
+
+    println!("{:?}", vec);
+}
