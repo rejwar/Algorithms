5db918c0235034834618941267d6f33b9f97fbf3
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-02-23 08:38:10 +0600

    Hei

---
+++ Array ADT/merged.rs
@@ -0,0 +1,19 @@
+// //concat
+
+// fn main() {
+//     let a = [1, 3];
+//     let b = [2, 4];
+
+//     let merged: Vec<i32> = [a, b].concat();
+
+//     println!("{:?}", merged);
+// }
+
+fn main() {
+    let a = [1, 3];
+    let b = [2, 4];
+
+    let merged: Vec<i32> = [a, b].concat();
+
+    println!(" {:?}", merged);
+}
